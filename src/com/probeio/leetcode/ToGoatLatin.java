d2449fb097b9b6c43e2a373d578d1342bace3019
probe <probe.park@snowcorp.com>
2020-06-03 13:49:18 +0900

    extract method

--- src/com/probeio/leetcode/ToGoatLatin.java
+++ src/com/probeio/leetcode/ToGoatLatin.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -13,15 +14,19 @@ public class ToGoatLatin {
         List<Character> vowelList = Arrays.asList('a', 'e', 'i', 'o', 'u');
         String[] chunks = word.split(" ");
         return IntStream.range(0, chunks.length)
-                .mapToObj(i -> {
-                    String chunk = chunks[i];
-                    if (vowelList.contains(chunk.toLowerCase().charAt(0))) {
-                        return String.join("", chunk, "ma", addA(i));
-                    } else {
-                        return String.join("", chunk.substring(1), String.valueOf(chunk.charAt(0)), "ma", addA(i));
-                    }
-                })
+                .mapToObj(transform(vowelList, chunks))
                 .collect(Collectors.joining(" "));
+    }
+
+    private IntFunction<String> transform(List<Character> vowelList, String[] chunks) {
+        return i -> {
+            String chunk = chunks[i];
+            if (vowelList.contains(chunk.toLowerCase().charAt(0))) {
+                return String.join("", chunk, "ma", addA(i));
+            } else {
+                return String.join("", chunk.substring(1), String.valueOf(chunk.charAt(0)), "ma", addA(i));
+            }
+        };
     }
 
     private String addA(int limit) {

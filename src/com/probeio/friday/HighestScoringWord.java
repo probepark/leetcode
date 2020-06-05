5f6df2113806d9f56c2e581fbe9b4d664161ece8
probe <re2rar@gmail.com>
2020-06-05 20:14:28 +0900

    a = 1 로 변경.

--- src/com/probeio/friday/HighestScoringWord.java
+++ src/com/probeio/friday/HighestScoringWord.java
@@ -8,11 +8,12 @@ import java.util.stream.Collectors;
 public class HighestScoringWord {
     public String highestScoringWord(String input) {
         return Arrays.stream(input.split(" "))
-                .collect(Collectors.toMap(s -> s.chars().sum(), o -> o))
+                .collect(Collectors.groupingBy(s -> s.chars().map(operand -> operand - 96).sum()))
                 .entrySet()
                 .stream()
                 .max(Comparator.comparingInt(Map.Entry::getKey))
                 .map(Map.Entry::getValue)
+                .flatMap(strings -> strings.stream().findAny())
                 .orElse("");
     }
 }
--- src/com/probeio/friday/HighestScoringWordTest.java
+++ src/com/probeio/friday/HighestScoringWordTest.java
@@ -17,4 +17,34 @@ public class HighestScoringWordTest {
         HighestScoringWord highestScoringWord = new HighestScoringWord();
         assertEquals("sentence", highestScoringWord.highestScoringWord("this sentence has two highest scoring words"));
     }
+
+    @Test
+    public void highestScoringWord3() {
+        HighestScoringWord highestScoringWord = new HighestScoringWord();
+        assertEquals("world", highestScoringWord.highestScoringWord("hello world"));
+    }
+
+    @Test
+    public void highestScoringWord4() {
+        HighestScoringWord highestScoringWord = new HighestScoringWord();
+        assertEquals("my", highestScoringWord.highestScoringWord("go ahead make my day"));
+    }
+
+    @Test
+    public void highestScoringWord5() {
+        HighestScoringWord highestScoringWord = new HighestScoringWord();
+        assertEquals("there", highestScoringWord.highestScoringWord("there is no place like home"));
+    }
+
+    @Test
+    public void highestScoringWord6() {
+        HighestScoringWord highestScoringWord = new HighestScoringWord();
+        assertEquals("aaaaaa", highestScoringWord.highestScoringWord("aaaaaa bbb cc f"));
+    }
+
+    @Test
+    public void highestScoringWord7() {
+        HighestScoringWord highestScoringWord = new HighestScoringWord();
+        assertEquals("bbb", highestScoringWord.highestScoringWord("bbb cc f aaaaaa"));
+    }
 }

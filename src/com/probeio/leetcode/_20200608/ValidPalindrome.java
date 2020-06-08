d7ee2a7a62486ff01d716b812991c248e092112d
probe <re2rar@gmail.com>
2020-06-08 12:05:19 +0900

    cleanup

--- src/com/probeio/leetcode/_20200608/ValidPalindrome.java
+++ src/com/probeio/leetcode/_20200608/ValidPalindrome.java
@@ -1,22 +1,11 @@
 package com.probeio.leetcode._20200608;
 
-import java.util.List;
-import java.util.regex.MatchResult;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
-        Pattern pattern = Pattern.compile("([a-zA-Z0-9])");
+        String words = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        List<String> words = pattern.matcher(s)
-                .results()
-                .map(MatchResult::group)
-                .map(String::toLowerCase)
-                .collect(Collectors.toList());
-
-        for (int i = 0, j = words.size() - 1; i < words.size(); i++, j--) {
-            if (!words.get(i).equals(words.get(j))) {
+        for (int i = 0, j = words.length() - 1; i < words.length(); i++, j--) {
+            if (!(words.charAt(i) == words.charAt(j))) {
                 return false;
             }
         }
